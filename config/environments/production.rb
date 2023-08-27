5e55e7a62eb01a93bcd265643359caeb254d3055
8tako8tako8 <8tako8tako8@gmail.com>
2023-08-27 17:05:48 +0900

    リダイレクトを許可する

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
+  # リダイレクトを許可する
+  config.action_controller.raise_on_open_redirects = false
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
