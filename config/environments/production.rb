b63af94124a0fd764c90783937eae78389dfce53
8tako8tako8 <8tako8tako8@gmail.com>
2023-08-27 15:08:30 +0900

    sendgridのセットアップ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'twitter-rails-api1-a77730c5e74f.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :domain => 'heroku.com',
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_APIKEY'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
