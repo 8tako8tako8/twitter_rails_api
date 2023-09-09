fa2b09a37d52199a794debb0f2e8f38d082b283f
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-09 19:01:54 +0900

    seedでUserとTweetのデータを作成したい

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do
+  USER_COUNT = 5
+  TWEET_COUNT = 5
+  users = []
+  USER_COUNT.times do |i|
+    user = User.new(
+      email: "user#{i}@example.com", password: 'password',
+      password_confirmation: 'password', name: "UserName#{i}", nickname: "UserNickname#{i}",
+      uid: SecureRandom.uuid, confirmed_at: Time.zone.now
+    )
+    user.save || puts(user.errors.full_messages)
+    users << user
+  end
+
+  TWEET_COUNT.times do |i|
+    users.each do |user|
+      user.tweets.create!(tweet: "#{user.name}によるツイートです。")
+    end
+  end
+end
