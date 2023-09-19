a95c43633b4864278c0564c924194e9ac7238b33
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-19 19:28:22 +0900

    会員登録時にユニークなユーザー名を割り振りたい

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -7,7 +7,17 @@ module Api
         private
 
         def sign_up_params
-          params.permit(:email, :password, :password_confirmation)
+          name = generate_unique_token(:name)
+          nickname = generate_unique_token(:nickname)
+          params.merge!(name:, nickname:)
+          params.permit(:email, :password, :password_confirmation, :name, :nickname)
+        end
+
+        def generate_unique_token(attribute)
+          loop do
+            token = SecureRandom.hex(8)
+            break token unless User.exists?(attribute => token)
+          end
         end
       end
     end
