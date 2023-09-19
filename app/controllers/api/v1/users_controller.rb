22841497db50bb0031fe946fc847a13d3a20454c
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-19 22:17:07 +0900

    ログインチェックする

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[show]
+
       def show
         @user = User.find(params[:id])
       end
