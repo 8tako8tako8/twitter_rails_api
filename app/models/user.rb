8458be4f6d81e3fbb6d31e73dd34d66dc004fd10
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-19 19:27:42 +0900

    userモデルのバリデーションを追加したい

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
   has_many :tweets, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :introduction, length: { maximum: 100 }
+  validates :location, length: { maximum: 10 }
+  validates :website_url, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, allow_blank: true
 end
