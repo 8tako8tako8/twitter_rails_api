6c8b01d6ef58502d7b69286c1a94b6c9e7c9c84f
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-23 19:32:27 +0900

    APIのformatをjsonに統一したい

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   namespace :api do
-    namespace :v1 do
+    namespace :v1, default: { format: 'json' } do
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
         resources :sessions, only: [:index]
       end
 
-      resources :users, only: %i[show], format: 'json'
-      resources :tweets, only: %i[index create show], format: 'json'
-      resources :images, only: [:create], format: 'json'
+      resources :users, only: %i[show]
+      resources :tweets, only: %i[index create show]
+      resources :images, only: [:create]
     end
   end
 end
