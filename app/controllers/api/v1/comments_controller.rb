2b019b73ef21c9ec4696b740f3a127797dec5ba6
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-10 08:04:17 +0900

    存在しないtweet_idの時にエラーハンドリングしたい

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,7 +6,12 @@ module Api
       before_action :authenticate_api_v1_user!, only: %i[create]
 
       def create
-        tweet = Tweet.find(comment_params[:tweet_id])
+        tweet = Tweet.find_by(id: comment_params[:tweet_id])
+        unless tweet
+          render json: { errors: 'ツイートが見つかりません' }, status: :not_found
+          return
+        end
+
         comment = current_api_v1_user.comment(comment_params[:comment], tweet)
         if comment.save
           render json: { comment: }, status: :created
