77314a7e74d9c7cb12ecb459c89223eb65d152cb
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-09 23:06:14 +0900

    コメントAPIを追加

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[create]
+
+      def create
+        pp comment_params[:comment]
+        pp comment_params[:tweet_id]
+        tweet = Tweet.find(comment_params[:tweet_id])
+        comment = current_api_v1_user.comment(comment_params[:comment], tweet)
+        if comment.save
+          render json: { comment: }, status: :created
+        else
+          render json: { errors: comment.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.permit(:comment, :tweet_id)
+      end
+    end
+  end
+end
