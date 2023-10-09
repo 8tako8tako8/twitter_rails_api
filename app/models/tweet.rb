7a2ac59b5060f6cebb8a5a11cde2a95df39109e4
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-09 23:03:55 +0900

    comment modelを追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments, dependent: :destroy
 
   validates :tweet, presence: true, length: { maximum: 200 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :avatar_image
   has_one_attached :header_image
 
