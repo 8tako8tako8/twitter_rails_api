d1b7bf94546fe2deb1937d45b8a40948591425c4
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-09 23:04:58 +0900

    user modelにcommentメソッドを追加してコメントを作成したい

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ApplicationRecord
   validates :location, length: { maximum: 10 }
   # TODO: サインイン時にallow_blankが適応されずバリデーションエラーとなるので一旦コメントアウト
   # validates :website_url, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, allow_blank: true
+
+  def comment(comment, tweet)
+    comments.create(comment: comment, tweet: tweet)
+  end
 end
