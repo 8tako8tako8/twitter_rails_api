0d98bad54261e88ef1cd28f7bdb1f57034a08119
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-14 23:28:29 +0900

    RetweetsControllerにログインチェックを追加

--- app/controllers/api/v1/retweets_controller.rb
+++ app/controllers/api/v1/retweets_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class RetweetsController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[create destroy]
 
       def create
         tweet = Tweet.find_by(id: params[:tweet_id])
