e3975edacdff1f92d5ea75625e4eda0ec61ae21f
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-22 00:48:25 +0900

    通知一覧APIを追加

---
+++ app/controllers/api/v1/notifications_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class NotificationsController < ApplicationController
+      include Pagination
+      before_action :authenticate_api_v1_user!, only: [:index]
+
+      def index
+        offset = params[:offset].presence || 1
+        limit = params[:limit].presence || 10
+        all_notifications = current_api_v1_user.notifications.order(created_at: :desc, id: :desc)
+
+        @notifications_paginated = all_notifications.page(offset).per(limit)
+        @pagination = pagination(@notifications_paginated)        
+        @user = current_api_v1_user
+      end
+    end
+  end
+end
