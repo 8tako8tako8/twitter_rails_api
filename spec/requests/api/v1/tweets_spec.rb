eb6e4c058fae2eaa4707b142d178630b41e789d9
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-21 21:28:59 +0900

    認証で落ちてたテストを修正する

--- spec/requests/api/v1/tweets_spec.rb
+++ spec/requests/api/v1/tweets_spec.rb
@@ -3,12 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Tweets', type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+
   # 認証をスキップさせる
   before do
     allow_any_instance_of(Api::V1::TweetsController).to receive(:authenticate_api_v1_user!).and_return(true)
+
+    # 認証情報をヘッダーに付与する
+    auth_token = user.create_new_auth_token
+    headers['access-token'] = auth_token['access-token']
+    headers['client'] = auth_token['client']
+    headers['uid'] = auth_token['uid']
   end
 
   describe 'GET /api/v1/tweets' do
+    let!(:user) { FactoryBot.create(:user) }
+
     before do
       FactoryBot.create_list(:tweet, 20)
     end
@@ -32,16 +42,7 @@ RSpec.describe 'Api::V1::Tweets', type: :request do
   describe 'POST /api/v1/tweets' do
     subject { post(api_v1_tweets_path, headers:, params:) }
 
-    let!(:user) { FactoryBot.create(:user) }
     let!(:params) { { tweet: 'テストツイート' } }
-
-    # 認証情報をヘッダーに付与する
-    before do
-      auth_token = user.create_new_auth_token
-      headers['access-token'] = auth_token['access-token']
-      headers['client'] = auth_token['client']
-      headers['uid'] = auth_token['uid']
-    end
 
     it 'ツイートが投稿できること' do
       subject
