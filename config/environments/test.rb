6b917542d3d4d7e7beb28e0240454d4a7a95328c
8tako8tako8 <8tako8tako8@gmail.com>
2023-10-22 01:29:55 +0900

    GitHub Actions上でメール送信時にテスト失敗するので修正してみる

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3100 }
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
