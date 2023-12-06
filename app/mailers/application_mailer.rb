241cbb7a095c53dda76bb8137bfe2e96aa3c0db4
8tako8tako8 <8tako8tako8@gmail.com>
2023-12-07 00:44:09 +0900

    rspecが起動できなかったので修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@#{ENV.fetch('FRONTEND_DOMAIN')}"
+  default from: ENV.fetch('RAILS_ENV') == 'production' ? "no-reply@#{ENV.fetch('FRONTEND_DOMAIN', nil)}" : ENV.fetch('SENDER_ADDRESS', nil)
   layout 'mailer'
 end
